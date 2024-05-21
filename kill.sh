3bcdca72d19d47edec15d068fb875c6398634e2a
DzikiSzogun <165959947+DzikiSzogun@users.noreply.github.com>
2024-05-22 00:43:23 +0200

    Update kill.sh - Auto disabling WAN and WIFI interface to prevent Internet access
    Auto disabling WAN and WIFI interface to prevent Internet access to prefent accidental PS4 system updates before jailbreaking
    
    Same as run.sh

--- kill.sh
+++ kill.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+# Disable WAN and WAN6 interface
+ifdown wan && ifdown wan6
+# Disable WiFi interface
+uci set wireless.radio0.disabled=1
+uci set wireless.radio1.disabled=1
+# Commit changes
+uci commit wireless
+# reload WiFI config				
+wifi reload
+# Commit changes
+/etc/init.d/network reload
+
 # Find all PIDs of processes containing 'myprocess' in their command line
 pids=$(ps | grep '[./]pppwn' | grep -v grep | awk '{print $1}')
 
